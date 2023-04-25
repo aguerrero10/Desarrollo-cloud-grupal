3f12a91178462cdb87eb9e4e850d49670f9f1b20
Javier <jaagomezmu@unal.edu.co>
2023-04-25 02:40:36 +0000

    Añadir requerimientos para ejecucion en VM

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -15,7 +15,8 @@ mail_settings = {
 def create_app(config_name):
     app = Flask(__name__)  
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tareas.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@proyecto.ckh1hljhxmxq.us-east-1.rds.amazonaws.com:5432/tareas'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@proyecto.ckh1hljhxmxq.us-east-1.rds.amazonaws.com:5432/tareas'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/tareas'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #app.config['FLASK_RUN_PORT'] = 5001
 
