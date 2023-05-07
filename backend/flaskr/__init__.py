b8c9af37f28b1bb5683e47562c9f879337062d06
Javier Alejandro Gómez Muñoz <92825836+jaagomezmu@users.noreply.github.com>
2023-05-06 19:45:21 -0500

    Update __init__.py
    Agregar URI de postgres de google

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -16,6 +16,7 @@ def create_app(config_name):
     app = Flask(__name__)  
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tareas.db'
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@proyecto.ckh1hljhxmxq.us-east-1.rds.amazonaws.com:5432/tareas'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@dsc-proyecto:us-central1:proyecto-sql/tareas'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #app.config['FLASK_RUN_PORT'] = 5001
 
